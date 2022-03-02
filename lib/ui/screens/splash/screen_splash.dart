5b8688efec132d4de18d5acbadacdc79cc5729a4
Sylvain <sylvain.duche@gmail.com>
2022-03-02 17:14:21 +0100

    feat(Theming): add theming for icon, title and splash screen

--- lib/ui/screens/splash/screen_splash.dart
+++ lib/ui/screens/splash/screen_splash.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:henri_potier/ui/controllers/controller_main.dart';
 import 'package:henri_potier/ui/screens/splash/controller/controller_splash.dart';
-import 'package:henri_potier/ui/widgets/common/widget_screen.dart';
-import 'package:henri_potier/ui/widgets/common/widget_text.dart';
+import 'package:henri_potier/utils/app_theme.dart';
 
 class ScreenSplash extends StatelessWidget {
   final controller = Get.put(ControllerSplash());
@@ -11,12 +10,18 @@ class ScreenSplash extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const WidgetScreen(
-      title: 'Splash',
-      canGoBack: false,
-      children: [
-        WidgetText('Splash'),
-      ],
+    return Scaffold(
+      backgroundColor: AppTheme.colorPrimary,
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset("assets/images/logo_big.png"),
+          ],
+        ),
+      ),
     );
   }
 }
