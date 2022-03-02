85054775f26549cdaa3f1889342a3e7bf319e11f
Sylvain <sylvain.duche@gmail.com>
2022-03-02 20:10:47 +0100

    feat(Basket): add icon on cover in bookList, to know if book is in basket

--- lib/ui/widgets/business/widget_book_cover.dart
+++ lib/ui/widgets/business/widget_book_cover.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:henri_potier/models/book.dart';
+import 'package:henri_potier/ui/controllers/controller_main.dart';
+import 'package:henri_potier/utils/app_theme.dart';
 
 class WidgetBookCover extends StatelessWidget {
+  final controllerMain = Get.find<ControllerMain>();
+
   final Book book;
-  const WidgetBookCover(this.book, {Key? key}) : super(key: key);
+
+  WidgetBookCover(this.book, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Stack(
       children: [
         Image.network(book.cover, fit: BoxFit.cover),
+        Positioned(
+          bottom: 8,
+          right: 8,
+          child: Obx(
+            () => controllerMain.isBookInBasket(book)
+                ? Container(
+                    decoration: BoxDecoration(color: AppTheme.colorPrimary, shape: BoxShape.circle),
+                    padding: EdgeInsets.all(12),
+                    child: SizedBox(
+                      width: 16,
+                      height: 16,
+                      child: Image.asset("assets/images/basket.png"),
+                    ),
+                  )
+                : Container(),
+          ),
+        )
       ],
     );
   }
